adf6c1850d4b9d5d1effca670dcf73b426b2f676
AdonisZK <adonisvefleyor@gmail.com>
2023-10-03 16:16:26 +0700

    update: model anime

--- app/Models/Anime.php
+++ app/Models/Anime.php
@@ -10,4 +10,11 @@ class Anime extends Model
     protected $table = 'anime';
 
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'cover_image',
+        'rating'
+    ];
 }
