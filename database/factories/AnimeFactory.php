4b35ec5fface3b9d10fe5017ab7d8decd51ec320
AdonisZK <adonisvefleyor@gmail.com>
2023-10-03 15:42:38 +0700

    add: model & factory & seeders

---
+++ database/factories/AnimeFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Anime>
+ */
+class AnimeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->text(500),
+            'cover_image' => $this->faker->imageUrl(),
+            'rating' => $this->faker->randomFloat(1, 1, 10),
+        ];
+    }
+}
