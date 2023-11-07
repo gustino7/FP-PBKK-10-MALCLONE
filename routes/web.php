70430fcabdebb4868b78a76c3f5fe8e9afa1f994
AdonisZK <adonisvefleyor@gmail.com>
2023-11-07 16:43:56 +0700

    update view: profile update

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/upload-profile-picture', [ProfileController::class, 'uploadProfilePicture'])->name('upload.profile_picture');
 });
 
 require __DIR__ . '/auth.php';
