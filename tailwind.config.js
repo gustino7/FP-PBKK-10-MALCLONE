368b12771e44d0d38b0a91ffb3db7dcd640e9356
AdonisZK <adonisvefleyor@gmail.com>
2023-11-07 10:23:48 +0700

    update: logo & colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'mal-blue': '#2F52A2',
+            },
         },
     },
 
