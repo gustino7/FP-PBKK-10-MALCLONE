4e35d4f8843834391a376d465858eb45f3e223aa
AdonisZK <adonisvefleyor@gmail.com>
2023-11-14 15:24:14 +0700

    update: individual anime page

--- resources/views/anime/show.blade.php
+++ resources/views/anime/show.blade.php
@@ -6,22 +6,40 @@
     </x-slot>
 
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-4">
-        <div class="bg-white p-6 shadow sm:rounded-lg">
-            @if (filter_var($anime->poster, FILTER_VALIDATE_URL))
-            <img src="{{ $anime->poster }}" alt="{{ $anime->title }}" class="w-48 h-72 object-cover rounded-md float-left mr-4 ml-2"> <!-- Add ml-2 for left margin -->
-            @else
-            <img src="{{ asset('storage/posters/' . $anime->poster) }}" alt="{{ $anime->title }}" class="w-48 h-72 object-cover rounded-md float-left mr-4 ml-2"> <!-- Add ml-2 for left margin -->
-            @endif
+        <div class="bg-white p-6 shadow sm:rounded-lg flex">
+            <div class="w-1/5">
+                @if (filter_var($anime->poster, FILTER_VALIDATE_URL))
+                <img src="{{ $anime->poster }}" alt="{{ $anime->title }}" class="w-full h-72 object-cover rounded-md mb-4">
+                @else
+                <img src="{{ asset('storage/posters/' . $anime->poster) }}" alt="{{ $anime->title }}" class="w-full h-72 object-cover rounded-md mb-4">
+                @endif
 
-            <div class="flex-1">
+                <div class="my-4 border-l-4 border-mal-blue pl-4">
+                    <p class="text-lg font-semibold text-black">Information</p>
+                    <p class="text-gray-600">
+                        <strong>Type:</strong> {{ $anime->type }}
+                    </p>
+                    <p class="text-gray-600">
+                        <strong>Episodes:</strong> {{ $anime->episode }}
+                    </p>
+                    <p class="text-gray-600">
+                        <strong>Premiered:</strong> {{ $anime->premiered }}
+                    </p>
+                    <p class="text-gray-600">
+                        <strong>Status:</strong> {{ $anime->status }}
+                    </p>
+                </div>
+            </div>
+
+            <div class="w-full ms-5">
                 <div class="font-bold border border-gray-300 p-2 mb-4 flex">
-                    <div class="w-1/7">
+                    <div class="w-1/8">
                         <div class="my-3 border-mal-blue pl-4">
-                            <p class="tet-center text-lg font-semibold text-mal-blue">SCORE</p>
-                            <p class="text-center text-3xl">{{ $anime->avg_rating }}</p>
+                            <p class="text-lg font-semibold text-mal-blue">SCORE</p>
+                            <p class="text-3xl">{{ $anime->avg_rating }}</p>
                         </div>
                     </div>
-                    <div class="w-1/4 ms-3">
+                    <div class="w-1/2 ms-3">
                         <div class="my-3 pl-4">
                             <p class="text-lg text-black mb-4">
                                 <span class="font-medium">Ranked :</span>
@@ -31,10 +49,8 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="flex-1">
                 <div class="font-medium border border-gray-300 p-2 mb-4 flex">
-                    <div class="w-1/7">
+                    <div class="w-full">
                         <div class="my-1 border-mal-blue pl-4">
                             <p class="text-m">
                                 <select class="border rounded px-2 py-1 me-3">
@@ -57,7 +73,6 @@
                                     <option value="2">(2) Horrible</option>
                                     <option value="1">(1) Appalling</option>
                                 </select>
-                                <span class="font-normal"></span>
                                 <select class="border rounded px-2 py-1 w-40">
                                     @for ($i = 0; $i <= $anime->episode; $i++)
                                         <option value="{{ $i }}">Episode: {{ $i }} / {{$anime->episode}}</option>
@@ -67,30 +82,13 @@
                         </div>
                     </div>
                 </div>
-
-                <p class="text-gray-700">
-                    <strong>Synopsis:</strong>
+                <p class="text-black">
+                    <strong>Synopsis</strong>
                 </p>
                 <div>
                     {{ $anime->synopsis }}
                 </div>
-                <div style="clear: both;"></div> <!-- Add this line to clear the floated image -->
-
-                <div class="my-4 border-l-4 border-mal-blue pl-4">
-                    <p class="text-lg font-semibold text-black">Information</p>
-                    <p class="text-gray-600">
-                        <strong>Type:</strong> {{ $anime->type }}
-                    </p>
-                    <p class="text-gray-600">
-                        <strong>Episodes:</strong> {{ $anime->episode }}
-                    </p>
-                    <p class="text-gray-600">
-                        <strong>Premiered:</strong> {{ $anime->premiered }}
-                    </p>
-                    <p class="text-gray-600">
-                        <strong>Status:</strong> {{ $anime->status }}
-                    </p>
-                </div>
             </div>
         </div>
+    </div>
 </x-app-layout>
