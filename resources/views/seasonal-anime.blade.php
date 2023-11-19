cd4a93625144284e13915bdfa9a1ba4b5a000e26
AdonisZK <adonisvefleyor@gmail.com>
2023-11-19 08:10:00 +0700

    update: seasonal anime

--- resources/views/seasonal-anime.blade.php
+++ resources/views/seasonal-anime.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-x-auto shadow-sm sm:rounded-lg p-6">
 
                 <!-- Seasonal Navigation -->
-                <div class="flex space-x-4 mb-4 justify-center">
+                <div class="flex space-x-4 mb-6 justify-center">
                     <a href="{{ route('anime.season', ['year' => $prevYear, 'season' => $prevSeason]) }}" class="border text-mal-blue px-4 py-2 rounded hover:bg-blue-500 hover:text-white">...</a>
                     @foreach($seasons as $season)
                     <a href="{{ route('anime.season', ['year' => $season['year'], 'season' => $season['season']]) }}" class="border text-mal-blue px-4 py-2 rounded hover:bg-blue-500 hover:text-white">{{ $season['label'] }}</a>
@@ -19,13 +19,12 @@
                 </div>
 
                 <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-
                     @foreach($seasonalAnimes as $anime)
-                    <div class="border border-gray-300 p-4 rounded-md mb-4 flex flex-col">
-                        <h2 class="text-lg font-semibold text-mal-blue text-center">
+                    <div class="border border-gray-300 py-4 rounded-md mb-4 flex flex-col">
+                        <h2 class="text-lg font-semibold text-mal-blue text-center h-12 mb-2 overflow-hidden flex items-center justify-center">
                             <a href="{{ route('anime.show', ['id' => $anime->id]) }}">{{ $anime->title }}</a>
                         </h2>
-                        <p class="text-sm text-gray-600 text-center">{{ $anime->premiered }} | Episode {{ $anime->episode }}</p>
+                        <p class="bg-gray-100 text-sm text-gray-600 text-center">{{ $anime->premiered }} | Episode {{ $anime->episode }}</p>
                         <div class="mt-4 flex-grow flex">
                             <img src="{{ filter_var($anime->poster, FILTER_VALIDATE_URL) ? $anime->poster : asset('storage/posters/' . $anime->poster) }}" alt="{{ $anime->title }}" class="w-1/2 h-64 object-cover rounded mb-2 mr-4">
                             <p class="text-sm text-gray-600 flex-grow">{{ $anime->synopsis }}</p>
@@ -33,7 +32,6 @@
                         <p class="text-sm text-gray-600">Average Rating: {{ $anime->avg_rating }}</p>
                     </div>
                     @endforeach
-
                 </div>
             </div>
         </div>
