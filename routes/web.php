3ff30a54196e62a41a9bb815e29b9b798cc4d08b
InoHex <mtgpmn2013@gmail.com>
2023-11-20 18:40:31 +0700

    feat: create review, update review, show rating avg

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimeController;
 
@@ -19,33 +20,54 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/anime', function () {
-    return view('anime');
-})->middleware(['auth', 'verified'])->name('anime');
+// Route::get('/anime', function () {
+//     return view('anime');
+// })->middleware(['auth', 'verified'])->name('anime');
 
-Route::get('/searchanime', function () {
-    return view('searchanime');
-})->middleware(['auth', 'verified'])->name('searchanime');
+// Route::get('/searchanime', function () {
+//     return view('searchanime');
+// })->middleware(['auth', 'verified'])->name('searchanime');
 
-Route::get('/topanime', [AnimeController::class, 'index'])->name('topanime');
-Route::get('/anime/create', [AnimeController::class, 'create'])->name('anime.create');
-Route::post('/anime', [AnimeController::class, 'store'])->name('anime.store');
-Route::get('/anime/{id}', [AnimeController::class, 'show'])->name('anime.show');
+// Route::get('/topanime', [AnimeController::class, 'index'])->name('topanime');
+// Route::get('/anime/create', [AnimeController::class, 'create'])->name('anime.create');
+// Route::post('/anime', [AnimeController::class, 'store'])->name('anime.store');
+// Route::get('/anime/{id}', [AnimeController::class, 'show'])->name('anime.show');
 
-Route::get('/anime/season/{year}/{season}', [AnimeController::class, 'seasonalAnime'])
-    ->where(['year' => '\d{4}', 'season' => 'winter|spring|summer|fall'])
-    ->name('anime.season');
+// Route::get('/anime/season/{year}/{season}', [AnimeController::class, 'seasonalAnime'])
+//     ->where(['year' => '\d{4}', 'season' => 'winter|spring|summer|fall'])
+//     ->name('anime.season');
 
 
 
-Route::get('/community', function () {
-    return view('community');
-})->middleware(['auth', 'verified'])->name('community');
+// Route::get('/community', function () {
+//     return view('community');
+// })->middleware(['auth', 'verified'])->name('community');
 
+
+Route::middleware(['auth','verified'])->group(function () {
+    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+    
+    // REVIEW
+    Route::get('/review/create/{anime_id}/{user_id}', [ReviewController::class, 'show'])->name('review.create');
+    Route::post('/review/create', [ReviewController::class, 'create']) ->name('review.store');
+    Route::put('/review/update', [ReviewController::class,'update']) ->name('review.update');
+    
+    // ANIME
+    Route::get('/anime', function () {return view('anime');})->name('anime');
+    Route::get('/searchanime', function () {return view('searchanime');})->name('searchanime');
+    Route::get('/topanime', [AnimeController::class, 'index'])->name('topanime');
+    Route::get('/anime/create', [AnimeController::class, 'create'])->name('anime.create');
+    Route::post('/anime', [AnimeController::class, 'store'])->name('anime.store');
+    Route::get('/anime/{id}', [AnimeController::class, 'show'])->name('anime.show');
+    Route::get('/anime/season/{year}/{season}', [AnimeController::class, 'seasonalAnime'])->where(['year' => '\d{4}', 'season' => 'winter|spring|summer|fall'])->name('anime.season');
+    
+    // Community
+    Route::get('/community', function () {return view('community');})->name('community');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
