1eb2b7eb323fe49faa9b157e57d87e8d66795f0f
Moch. Taslam Gustino P <93267604+gustino7@users.noreply.github.com>
2023-12-03 13:23:51 +0700

    feat: notification create anime (#21)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,46 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        {{-- Jquery --}}
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+        {{-- Library Alert --}}
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+
+        {{-- Pusher --}}
+        <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
+        <script>
+            // Enable pusher logging - don't include this in production
+            Pusher.logToConsole = true;
+
+            var pusher = new Pusher('0f26b5a42eafd91ac7cb', {
+                cluster: 'ap1'
+            });
+
+            var channel = pusher.subscribe('notification-channel');
+            channel.bind('notification-anime', function(data) {
+                let anime = JSON.stringify(data.title);
+                let user = JSON.stringify(data.user);
+
+                @php
+                    if(auth()->check()){
+                        $userNotif = Auth::user();
+                        $roleNotif = $userNotif -> isAdmin;
+                    }else{
+                        $roleNotif = 0;
+                    }
+                @endphp
+                
+                let role = @json($roleNotif);
+                console.log(role);
+                if(role === 1){
+                    toastr.success('Anime ' + anime + ' has been added by ' + user, 'Notification');
+                }
+            });
+        </script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
